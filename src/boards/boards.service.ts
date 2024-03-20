55af2a7b8cb39707728fee7d634d60b8bbfe11c7
변민우 <yja05092@gmail.com>
2024-03-20 23:10:17 +0900

    특정 게시물을 찾을 때 없는 경우 결과 값 처리

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v1 as uuid } from 'uuid';
 import { createBoardDto } from './dto/create-board.dto';
@@ -24,7 +24,12 @@ export class BoardsService {
   }
 
   getBoardById(id: string): Board {
-    return this.boards.find((board) => board.id === id);
+    const found = this.boards.find((board) => board.id === id);
+    if (!found)
+      throw new NotFoundException(
+        `'${id}' id를 가진 게시물은 존재하지 않습니다.`,
+      );
+    return found;
   }
 
   updateBoardStatus(id: string, status: BoardStatus): Board {
