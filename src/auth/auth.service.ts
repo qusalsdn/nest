70854267beedbad3111621adcf25de870e5c1afe
변민우 <yja05092@gmail.com>
2024-03-21 17:10:41 +0900

    인증 기능 구현 준비

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+}
